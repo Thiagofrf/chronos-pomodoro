d0b28137d3dc67184d3d15f21c7ab12345e0884f
Thiago <thiago.fontanella@hotmail.com>
2026-01-07 22:12:07 -0300

    feat: finish heading structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { Timer } from 'lucide-react';
 
 function App() {
   return (
     <>
-      <Heading>Olá mundo!</Heading>
+      <Heading>
+        Olá mundo!
+        <button>
+          <Timer />
+        </button>
+      </Heading>
       <p>Lorem ipsum dolor sit amet</p>
     </>
   );
