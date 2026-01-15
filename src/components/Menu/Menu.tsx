2d816fc9b53cde9809144465cf15f1ba6df796fe
Thiago <thiago.fontanella@hotmail.com>
2026-01-14 22:51:52 -0300

    feat: create light theme variables; apply useState hook on Menu to deal with theme changing

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,22 +1,49 @@
 import styles from './Menu.module.css';
+import { useState } from 'react';
 
 import { MenuLink } from '../MenuLink/MenuLink';
 import {
   HistoryIcon,
   SettingsIcon,
   SunIcon,
-  // MoonIcon,
+  MoonIcon,
   HouseIcon,
 } from 'lucide-react';
 
 export function Menu() {
+  const [theme, setTheme] = useState<'light' | 'dark'>('dark');
+
+  function handleThemeChange(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
   return (
     <nav className={styles.menuContainer}>
-      <MenuLink icon={<HouseIcon />} link='#' />
-      <MenuLink icon={<HistoryIcon />} link='#' />
-      <MenuLink icon={<SettingsIcon />} link='#' />
-      <MenuLink icon={<SunIcon />} link='#' />
-      {/* <MenuLink icon={<MoonIcon />} link='#' /> */}
+      <MenuLink icon={<HouseIcon />} link='#' aria-label='Home' title='Home' />
+      <MenuLink
+        icon={<HistoryIcon />}
+        link='#'
+        aria-label='History'
+        title='History'
+      />
+      <MenuLink
+        icon={<SettingsIcon />}
+        link='#'
+        aria-label='Settings'
+        title='Settings'
+      />
+      <MenuLink
+        icon={theme === 'dark' ? <SunIcon /> : <MoonIcon />}
+        link='#'
+        aria-label='Change theme'
+        title='Change theme'
+        onClick={handleThemeChange}
+      />
     </nav>
   );
 }
--- src/components/MenuLink/MenuLink.tsx
+++ src/components/MenuLink/MenuLink.tsx
@@ -3,11 +3,11 @@ import styles from './MenuLink.module.css';
 type MenuLinkProps = {
   icon: React.ReactNode;
   link: string;
-};
+} & React.ComponentProps<'a'>;
 
-export function MenuLink({ icon, link }: MenuLinkProps) {
+export function MenuLink({ icon, link, ...props }: MenuLinkProps) {
   return (
-    <a className={styles.menuLink} href={link}>
+    <a className={styles.menuLink} href={link} {...props}>
       {icon}
     </a>
   );
