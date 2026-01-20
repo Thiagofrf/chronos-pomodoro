6eb695b519e2e1113c5c9883e458b7e0b73fffb8
Thiago <thiago.fontanella@hotmail.com>
2026-01-19 23:01:47 -0300

    fix: remove unused const from app

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,6 @@ import { TaskStateModel } from './models/TaskStateModel';
 import './styles/theme.css';
 import './styles/global.css';
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
-
 function App() {
   return (
     <>
