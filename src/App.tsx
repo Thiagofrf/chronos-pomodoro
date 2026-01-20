37ac6491ee223dd2b7198d075de70d4979d88d50
Thiago <thiago.fontanella@hotmail.com>
2026-01-19 23:37:53 -0300

    fix: remove unused imports and code from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react';
-
 import { Container } from './components/Container/Container';
 import { Home } from './pages/Home/Home';
-import { TaskStateModel } from './models/TaskStateModel';
 
 import './styles/theme.css';
 import './styles/global.css';
