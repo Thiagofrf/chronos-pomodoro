8fcf602d64d1c2309bbff37f72e137724f7f93b2
Thiago <thiago.fontanella@hotmail.com>
2026-01-19 23:01:24 -0300

    fix: adjust type key name

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import './styles/theme.css';
-import './styles/global.css';
+import { useState } from 'react';
 
 import { Container } from './components/Container/Container';
 import { Home } from './pages/Home/Home';
+import { TaskStateModel } from './models/TaskStateModel';
+
+import './styles/theme.css';
+import './styles/global.css';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 function App() {
   return (
