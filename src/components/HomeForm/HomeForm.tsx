bbab3b29ed2a342a45ae0bb1d3f1161d7ec03fef
Thiago <thiago.fontanella@hotmail.com>
2026-01-20 23:17:39 -0300

    feat: apply handle functions to deal with tasks form

--- src/components/HomeForm/HomeForm.tsx
+++ src/components/HomeForm/HomeForm.tsx
@@ -1,18 +1,27 @@
-import styles from './HomeForm.module.css';
-
+import { useState } from 'react';
 import { CirclePlay, StopCircleIcon } from 'lucide-react';
 import { Input } from '../Input/Input';
 import { Cycles } from '../Cycles/Cycles';
 import { DefaultButton } from '../DefaultButton/DefaultButton';
 
+import styles from './HomeForm.module.css';
+
 export function HomeForm() {
+  const [taskName, setTaskName] = useState('');
+
+  const handleCreateTaskForm = (event: React.FormEvent<HTMLElement>) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className={styles.homeForm} action=''>
+    <form className={styles.homeForm} action='' onSubmit={handleCreateTaskForm}>
       <Input
         id='taskInput'
         type='text'
         placeholder='Ex.: Estudar para a prova'
         labelText='Task:'
+        value={taskName}
+        onChange={e => setTaskName(e.target.value)}
       />
       <p className={styles.formHelper}>
         Nesse ciclo <b>descanse</b> por <b>5 min.</b>
