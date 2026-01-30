f6dc7c9edb03b81d9e6ae493252e32542c843b37
Thiago <thiago.fontanella@hotmail.com>
2026-01-29 23:06:18 -0300

    feat: start using web worker

--- src/components/HomeForm/HomeForm.tsx
+++ src/components/HomeForm/HomeForm.tsx
@@ -42,6 +42,12 @@ export function HomeForm() {
     };
 
     dispatch({ type: TaskActionsTypes.START_TASK, payload: newTask });
+
+    const worker = new Worker(
+      new URL('../../workers/timerWorker.js', import.meta.url),
+    );
+
+    worker.postMessage('Olá, worker! Inicie o temporizador.');
   };
 
   function handleInterruptTask() {
