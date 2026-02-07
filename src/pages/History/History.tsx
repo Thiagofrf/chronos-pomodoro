720e1faac38f2918bc1bb60657a8de655f993d31
Thiago <thiago.fontanella@hotmail.com>
2026-02-07 16:03:57 -0300

    feat: get task status based on complete or interrupted dates

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -6,6 +6,7 @@ import { MainTemplate } from '../../templates/MainTemplate/MainTemplate';
 import styles from './History.module.css';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { formatDate } from '../../utils/formatDate';
+import { getTaskStatus } from '../../utils/getTaskStatus';
 
 export function History() {
   const { state } = useTaskContext();
@@ -43,7 +44,7 @@ export function History() {
                   <td>{task.name}</td>
                   <td>{task.duration}</td>
                   <td>{formatDate(task.startDate)}</td>
-                  <td>{task.interruptedDate}</td>
+                  <td>{getTaskStatus(task, state.activeTask)}</td>
                   <td>{task.type}</td>
                 </tr>
               );
